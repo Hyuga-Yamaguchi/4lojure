84f97ef96add3fc0d96896260165b78de232f06f
hyamaguchi <hyamaguchi@xcoo.jp>
2025-05-05 22:15:49 +0900

    q85

--- src/q85.clj
+++ src/q85.clj
@@ -1,1 +1,22 @@
 (ns q85)
+
+(defn main
+  []
+  #(let [n (count %)
+         total (bit-shift-left 1 n)
+         indexed-item (vec %)]
+     (set
+      (for [mask (range total)]
+        (->> (range n)
+             (filter (fn [i] (bit-test mask i)))
+             (map indexed-item)
+             set)))))
+
+(= ((main) #{1 :a}) #{#{1 :a} #{:a} #{} #{1}})
+
+(= ((main) #{}) #{#{}})
+
+(= ((main) #{1 2 3})
+   #{#{} #{1} #{2} #{3} #{1 2} #{1 3} #{2 3} #{1 2 3}})
+
+(= (count ((main) (into #{} (range 10)))) 1024)
